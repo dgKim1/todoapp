044d09500e4ce7ab5918f30f85595999ada4abe7
dgKim1 <kimdg3556@naver.com>
2024-07-01 13:12:12 +0900

    finish for level 4 of todo app

--- main.js
+++ main.js
@@ -7,28 +7,70 @@ addBttn.addEventListener("click",addTask);
 
 
 function addTask() {
-    let taskContent = taskInput.value;
-    taskList.push(taskContent);
-    console.log(taskContent);
+  let task = {
+    id: randomIdGenerate(),
+    taskContent: taskInput.value,
+    isComplete: false
+  }
+    taskList.push(task);
+    console.log(taskList);
     render();
 }
 
 function render(){
     let resultHtml = "";
     for (let i=0; i<taskList.length;i++){
+      if(taskList[i].isComplete == true){
         resultHtml += `
         <div class="task">
-            <div>${taskList[i]}</div>
+            <div class="task-done">${taskList[i].taskContent}</div>
             <div>
-              <button>check</button>
-              <button>delete</button>
+              <button onClick="toggleComplete('${taskList[i].id}')">check</button>
+              <button onClick="deleteTask('${taskList[i].id}')">Delete</button>
             </div>
-          </div>
-        `;
+          </div>`;
+      } else{
+        resultHtml +=
+        `
+        <div class="task">
+            <div>${taskList[i].taskContent}</div>
+            <div>
+              <button onClick="toggleComplete('${taskList[i].id}')">check</button>
+              <button onClick="deleteTask('${taskList[i].id}')">Delete</button>
+            </div>
+          </div>`;
+      }
     }
 
 
     document.getElementById("task-board").innerHTML = resultHtml;
 
 
+}
+
+
+
+function toggleComplete(id) {
+  for(let i=0;i<taskList.length;i++){
+    if(taskList[i].id == id){
+      taskList[i].isComplete = !taskList[i].isComplete;
+      break;
+    }
+  }
+  render();
+}
+
+
+function deleteTask(id){
+  for(let i=0;i<taskList.length;i++){
+    if(taskList[i].id == id){
+      taskList.splice(i,1);
+      break;
+    }
+  }
+  render();
+}
+
+function randomIdGenerate() {
+  return "_"+Math.random().toString(36).substr(2, 9);
 }
