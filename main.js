06b7ec82c5012485efc5628a703a2da0d0450589
dgKim1 <kimdg3556@naver.com>
2024-07-04 23:29:32 +0900

    make icons for check

--- main.js
+++ main.js
@@ -5,9 +5,9 @@ let tabs = document.querySelectorAll(".task-tabs div");
 let filterList = [];
 let mode = "all";
 let underLine = document.getElementById("under-line");
+let check = true;
 
 addBttn.addEventListener("click", addTask);
-
 
 //기본 underline style 설정 (all탭 아래)
 underLine.style.left = tabs[1].offsetLeft + "px";
@@ -65,8 +65,15 @@ function render() {
         <div class="task">
             <div class="task-done">${list[i].taskContent}</div>
             <div>
-              <button onClick="toggleComplete('${list[i].id}')">check</button>
-              <button onClick="deleteTask('${list[i].id}')">Delete</button>
+            <svg onClick="toggleComplete('${list[i].id}')" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-clockwise" viewBox="0 0 16 16">
+          <path fill-rule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2z"/>
+          <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466"/>
+        </svg> 
+              
+        <svg id="delete-button" xmlns="http://www.w3.org/2000/svg" onClick="deleteTask('${list[i].id}')" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+  <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
+  <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
+</svg>
             </div>
           </div>`;
     } else {
@@ -74,8 +81,15 @@ function render() {
         <div class="task">
             <div>${list[i].taskContent}</div>
             <div>
-              <button onClick="toggleComplete('${list[i].id}')">check</button>
-              <button onClick="deleteTask('${list[i].id}')">Delete</button>
+           <svg onClick="toggleComplete('${list[i].id}')" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-square" viewBox="0 0 16 16">
+            <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"/>
+            <path d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425z"/>
+          </svg>
+              
+          <svg id="delete-button" xmlns="http://www.w3.org/2000/svg" onClick="deleteTask('${list[i].id}')" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+    <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
+    <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
+  </svg>
             </div>
           </div>`;
     }
@@ -86,34 +100,29 @@ function render() {
 
 function toggleComplete(id) {
   for (let i = 0; i < taskList.length; i++) {
-    if (taskList[i].id == id) {
+    if (taskList[i].id === id) {
       taskList[i].isComplete = !taskList[i].isComplete;
       break;
     }
   }
-  render();
+  filter();
 }
 
 function deleteTask(id) {
   for (let i = 0; i < taskList.length; i++) {
-    if (taskList[i].id == id) {
+    if (taskList[i].id === id) {
       taskList.splice(i, 1);
       break;
     }
   }
-  if (mode !== "all") {
-    for (let i = 0; i < filterList.length; i++) {
-      if (filterList[i].id == id) {
-        filterList.splice(i, 1);
-        break;
-      }
-    }
-  }
-  render();
+  filter();
 }
 
 function filter(event) {
-  mode = event.target.id;
+  if(event){
+    mode = event.target.id;
+  }
+  
   filterList = [];
   if (mode === "all") {
     render();
@@ -130,6 +139,9 @@ function filter(event) {
         filterList.push(taskList[i]);
       }
     }
+    render();
+  }
+  else{
     render();
   }
 }
